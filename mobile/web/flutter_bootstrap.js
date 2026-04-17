7e07daeaba0a312024c881556641ac8f1f1ead9c
jomarsv <jomarsv@gmail.com>
2026-04-17 17:45:24 -0300

    disable stale web service worker cache

---
+++ mobile/web/flutter_bootstrap.js
@@ -0,0 +1,12 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+_flutter.loader.load();
